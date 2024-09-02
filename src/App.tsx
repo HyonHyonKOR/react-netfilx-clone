f3de7f5134da359aefff1da1565bd5836529fd03
HyonHyon <khm3626@gmail.com>
2024-09-02 21:41:34 +0900

    feat:variants part two(delayChildren, staggerChildren

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { addScaleCorrector, delay, motion } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -12,24 +12,60 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 0.2);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
   end: {
+    opacity: 1,
     scale: 1,
-    rotateZ: 360,
-    transition: { type: "spring", stiffness: 260, damping: 20 },
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants}></Circle>
+        <Circle variants={circleVariants}></Circle>
+        <Circle variants={circleVariants}></Circle>
+        <Circle variants={circleVariants}></Circle>
+      </Box>
     </Wrapper>
   );
 }
