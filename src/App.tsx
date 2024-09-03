c6417a06cd4eb6e320ed1153e390cf3d314eac5f
HyonHyonKOR <khm3626@gmail.com>
2024-09-03 13:23:07 +0900

    feat:motion value part three

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import styled from "styled-components";
-import {
-  motion,
-  useMotionValue,
-  useMotionValueEvent,
-  useTransform,
-} from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
+  background: linear-gradient(135deg, #e09, #d0e);
 `;
 
 const Box = styled(motion.div)`
@@ -24,11 +20,20 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
+  const rotate = useTransform(x, [-800, 800], [-360, 360]);
+  const background = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
 
   return (
-    <Wrapper>
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin></Box>
+    <Wrapper style={{ background: background }}>
+      <Box style={{ x, rotateZ: rotate }} drag="x" dragSnapToOrigin></Box>
     </Wrapper>
   );
 }
